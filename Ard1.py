4b058b494d57ae85b22b392b4584d8f6a3f9900f
aquarcoo <114112756+aquarcoo@users.noreply.github.com>
2022-10-27 16:29:12 -0500

    Add files via upload

--- Ard1.py
+++ Ard1.py
@@ -12,11 +12,23 @@ import time   #import time functions to have delays in commands that the pi send
 if __name__ == '_main_' : 
     ser = s.Serial('/dev/ttyACM0', 9600, timeout = 1) #Check if adruino is ACM1 or ACM0 by using powershell of Raspberry Pi 
     ser.flush()  
-    
+     
+    def sendInt(servo_angle):
+        send_string=str(servo_angle)
+        send_string += "\n"
+        ser.write(send_string.encode('utf-8'))
+        ser.flush()
+
+
+        sendInt(60) #send 60 deg to servo
+        time.sleep(2.5)
+        sendInt(0) #send 0 to servo to stop the DCmotor and set servo to 90 deg 
+        
     while True: 
         if ser.in_waiting > 0: 
             line = ser.readline().decode('utf-8').rstrip() 
             print(line) 
             if float(line) <= 60: 
-                ser.write(b"myservo.write(pos)\n") 
+                ser.write(b"myservo.write(pos)\n")   
+                
                 
